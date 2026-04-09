0851d9873458d28a28b54965d9041efce7e71b74
Saumya Verma <2k22.cse.2213318@gmail.com>
2026-04-09 12:14:07 +0530

    fix routing for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
